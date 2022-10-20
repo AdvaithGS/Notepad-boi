fcce7c4b4e356275a7ca78f0fe6bae8b34c89a3f
Advaith GS <advaith.gs4@gmail.com>
2022-10-20 19:00:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -227,5 +227,5 @@ async def on_message(message):
   
 
 keep_alive()
-client.run('ODA4MjYyODAzMjI3NDEwNDY1.YCD_ZQ.EDL3Sg03X1VU6zk2sqEyMtA-JPQ')# lmao use this
+client.run(os.environ['notepad_boi'])
 #dark blue,
